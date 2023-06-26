d308adb5edc60f28212010d1ccdfcc51ad8e393e
seungji <dkstmdwl0615@naver.com>
2023-06-26 15:16:39 +0900

    영화 상세정보 페이지 임의의 영화를 값으로 구조 개설 및 스타일링

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "pages/Detail";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/detail/1" element={<Detail />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- movie/src/pages/detail.jsx
+++ movie/src/pages/detail.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import list_movies from "../data/list_movies.json";
+import styles from "styles/detail.css";
 
 const detail = () => {
-  return;
+  const movie = list_movies.data.movies[0];
+  //   console.log(Object.keys(movie));
+  //   console.log(Object.values(movie));
+  return (
+    <article>
+      <h2>{movie.title}</h2>
+      <img src={movie.large_cover_image} alt={movie.title + "'s cover"} />
+      <h3>synopsis</h3>
+      <span>{movie.synopsis}</span>
+      <h3>summary</h3>
+      <p>{movie.summary}</p>
+      <div className="desc_wrap">
+        <h3>title : {movie.title_long}</h3>
+        <span>id:{movie.id}</span>
+        <span>year : {movie.year}</span>
+        <span>rating : {movie.rating}</span>
+        <span>runtime : {movie.runtime}</span>
+        <span>genres : {movie.genres}</span>
+        <p>{movie.description_full}</p>
+      </div>
+      <p className="below">date_uploaded : {movie.date_uploaded}</p>
+    </article>
+  );
 };
 
-export default detail
+export default detail;
