055f2d62cedfaed2be38e7267f4511885fab7004
seungji <dkstmdwl0615@naver.com>
2023-06-26 16:21:18 +0900

    유은님 메인 페이지 구현에 따른 구조 수정중

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./routes/home";
+import Detail from "./routes/detail";
 // import "./App.css";
 
 const App = createBrowserRouter(
