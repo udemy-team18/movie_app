aeab50ce6752c16cd8eb38ace02da3ca96c11a6a
leg1771 <leg1770@naver.com>
2023-06-28 00:01:20 +0900

    검색 초기버전

---
+++ movie/src/components/Search.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import styles from "styles/css/Search.module.css";
+
+
+const Search = ({object, getData})=>{
+    const [value,setValue] = useState('');
+    
+    
+    const handleChange = (e)=>{
+        setValue(e.target.value.toLowerCase())
+
+      
+        
+    }
+
+
+
+
+    const filter = ()=>{
+       const name = object.filter((item)=>
+        item.title.toLowerCase().includes(value)
+        )
+
+        
+        getData(name);
+
+        
+        
+    }
+
+
+    return (
+
+                
+                <div className = {styles.search}>
+                    <input className = {styles.input} placeholder = "영화명을 입력하세요" onChange = {handleChange}></input>
+                    <FaMagnifyingGlass className = {styles.icon} onClick = {filter}/>
+                </div> 
+    )
+}
+
+export default Search;
--- movie/src/routes/home.jsx
+++ movie/src/routes/home.jsx
@@ -4,10 +4,20 @@ import Main from "../pages/main";
 import Navi from "../components/navigation";
 import Footer from "../components/footer";
 import style from "../styles/css/main.module.css";
+import Search from "../components/Search"
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState();
+  const [input,setInput] = useState('')
+  const[child,setChild] = useState('');
+
+  const getData = (child)=>{
+    setChild(child);
+    
+  }
+
+  console.log(`${child} : 잘 됐다면 여기에 배열이 뜰꺼야`)
   const getMovies = async () => {
     const json = await (
       await fetch(
@@ -16,19 +26,23 @@ function Home() {
     ).json();
     setMovies(json.data.movies);
     setLoading(false);
+    
   };
   useEffect(() => {
     getMovies();
   }, []);
 
-  console.log(movies);
+  
 
   return (
     <div>
       <Navi />
+    
+      <Search object = {movies} getData = {getData}/>
+
       {loading ? (
         <h1 className={style.loading}>loading...</h1>
-      ) : (
+      ) : child === '' ? (
         <div className={style.container}>
           {movies.map((movie) => (
             <Main
@@ -43,7 +57,25 @@ function Home() {
             />
           ))}
         </div>
-      )}
+      ) : (
+        <div className={style.container}>
+          {child.map((movie) => (
+            <Main
+              movie={movie}
+              key={movie.id}
+              id={movie.id}
+              coverImage={movie.medium_cover_image}
+              title={movie.title}
+              year={movie.year}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )
+      
+      
+      }
       <Footer />
     </div>
   );
