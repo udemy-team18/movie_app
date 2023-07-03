314eac3687c9e3651386257467c71d7ef09da5d5
seungji <dkstmdwl0615@naver.com>
2023-07-03 14:06:59 +0900

    redux converting working : need to study

--- movie/src/index.js
+++ movie/src/index.js
@@ -14,9 +14,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider>
+  <Provider store={store}>
     <RouterProvider router={App} />
   </Provider>
 );
